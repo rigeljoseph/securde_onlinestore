f56e3e1b544cbe350488184b2c3a2d8e9e520c21
tokii <tokiinyan@gmail.com>
2018-04-07 17:23:10 +0800

    pahabol Eloquent pt. 2

---
+++ app/Address.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    //
+
+    protected $table = "addresses";
+
+    protected $primaryKey = "address_id";
+
+    protected $keyType = "string";
+
+    protected $incrementing = false;
+
+    public $timestamps = true;
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+}
---
+++ app/Category.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $table = "categories";
+
+    protected $primaryKey = "category_id";
+
+    public $timestamps = true;
+
+    public function item(){
+        return $this->belongsTo('App\Item');
+    }
+}
