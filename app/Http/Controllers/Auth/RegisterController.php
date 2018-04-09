e7777179fc1ee1d13fe7323d9073c6c43c540e71
tokii <tokiinyan@gmail.com>
2018-04-10 01:31:15 +0800

    regular expressions for input validation on register page

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,10 +49,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'username' => 'required|string|min:3|max:20|unique:users',
             'email' => 'required|string|email|max:255|unique:users',
-            'first_name' => 'required|string|max:255|',
-            'last_name' => 'required|string|max:255|',
             'password' => 'required|string|min:6|case_diff|numbers|letters|symbols|confirmed',
         ]);
     }
