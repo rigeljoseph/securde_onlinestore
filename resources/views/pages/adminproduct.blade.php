d8dc528f414c67eb8a68ca83d82be7c0348eca8e
tokii <tokiinyan@gmail.com>
2018-04-09 18:20:44 +0800

    changed product view layout
    modified the one from user roles

--- resources/views/pages/adminproduct.blade.php
+++ resources/views/pages/adminproduct.blade.php
@@ -1,58 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
- 
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header">{{ __('Item List') }}</div>
 
-<h1 class="display-4 text-center">List Of Items</h1>
+          <div class="card-body">
+              <button type="button" onclick="" class="btn btn-secondary">Create Item</button>
 
-<div class="modal-body row" style="margin-right:20px; margin-left:400px;margin-top:75px;">
-<div class="col-md-8">
-  <!-- Your first column here -->
-  <table class="table">
-  <thead class="table-primary">
-    <tr>
-      <th scope="col">Items</th>
-      <th scope="col">Price</th>
-      <th scope="col">Quantity</th>
-      <th scope="col"></th>
-      <th scope="col"></th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">cool shirt</th>  <!-- insert from cart loop-->
-      <td>20 pesos</td>
-      <td>3</td>
-      <td><button type="button" class="btn btn-outline-danger"> Remove</button></td>  <!-- delete item-->
-      <td><button type="button" class="btn btn-outline-info"> Edit</button></td>  <!-- edit item-->
-    </tr>
-    <tr>
-      <th scope="row">cool pants</th>
-      <td>20 pesos</td>
-      <td>3</td>
-      <td><button type="button" class="btn btn-outline-danger">Remove</button></td>  <!-- delete item-->
-      <td><button type="button" class="btn btn-outline-info"> Edit</button></td>  <!-- edit item-->
-    </tr>
-    <tr>
-    <th scope="row">another cool shirt</th>
-    <td>20 pesos</td>
-    <td>3</td>
-    <td><button type="button" class="btn btn-outline-danger">Remove</button></td>  <!-- delete item-->
-    <td><button type="button" class="btn btn-outline-info"> Edit</button></td>  <!-- edit item-->
-    </tr>
+              <div class="p-2"></div>
+            <table class="table">
 
-    <tr>
-    <th ></th>
-    <td></td>
-    <td></td>
-    <td>
-    <td><button type="button" class="btn btn-outline-success"> Add Item</button></td>  <!-- edit item-->
-    </tr>
-  </tbody>
-</table> 
-</div>
+                <thead class="table-primary">
+                <tr>
+                  <th scope="col">SKU</th>
+                  <th scope="col">Item</th>
+                  <th scope="col">Category</th>
+                  <th scope="col"></th>
+                  <th scope="col"></th>
+                </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
 
-
-</div>
+          </div>
+        </div>
+      </div>
+    </div>
 
 @endsection
