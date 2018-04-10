15224bf44d6abc3a23768ea3fb47fb1abb0d446c
tokii <tokiinyan@gmail.com>
2018-04-10 23:13:22 +0800

    modifed admin invoice view
    gooodnight!

--- resources/views/pages/adminpurchases.blade.php
+++ resources/views/pages/adminpurchases.blade.php
@@ -1,56 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
- 
-
-<h1 class="display-4 text-center">List Of Orders</h1>
-
-
-<div class="modal-body row" style="margin-right:20px; margin-left:400px;margin-top:100px;">
-<div class="col-md-8">
-  <!-- Your first column here -->
-  <table class="table">
-  <thead class="table-primary">
-    <tr>
-      <th scope="col">Purchase ID</th>
-      <th scope="col">Customer Name</th>
-      <th scope="col">Order Date</th>
-      <th scope="col">Total Cost</th>
-      <th scope="col"></th>
-      <th scope="col"></th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1241</th>  <!-- insert from cart loop-->
-      <td>John</td>
-      <td>april 7, 2018</td>
-      <td>100 pesos</td>
-      <td><button type="button" class="btn btn-outline-success"> Accept</button></td>  <!-- Accept order-->
-      <td><button type="button" class="btn btn-outline-danger"> Reject</button></td>  <!--Reject order-->
-    </tr>
-    <tr>
-    <th scope="row">1242</th>  <!-- insert from cart loop-->
-    <td>Johnny</td>
-    <td>april 7, 2018</td>
-    <td>120 pesos</td>
-    <td><button type="button" class="btn btn-outline-success"> Accept</button></td>  <!-- Accept order-->
-    <td><button type="button" class="btn btn-outline-danger"> Reject</button></td>  <!--Reject order-->
-  </tr>
-  <tr>
-  <th scope="row">1243</th>  <!-- insert from cart loop-->
-  <td>Johnloyd</td>
-  <td>april 7, 2018</td>
-  <td>130 pesos</td>
-  <td><button type="button" class="btn btn-outline-success"> Accept</button></td>  <!-- Accept order-->
-  <td><button type="button" class="btn btn-outline-danger"> Reject</button></td>  <!--Reject order-->
-</tr>
-
-    </tbody>
-</table> 
-</div>
-
-
-</div>
-
-@endsection
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header">{{ __('Invoice List') }}</div>
+          <div class="card-body">
+            <div class="p-2"></div>
+            <table class="table">
+                <thead class="table-primary">
+                <tr>
+                  <th scope="col">Purchase ID</th>
+                  <th scope="col">Customer Name</th>
+                  <th scope="col">Address</th>
+                  <th scope="col">Total Cost</th>
+                  <th scope="col">Order Date</th>
+                  <th scope="col"></th>
+                  <th scope="col"></th>
+                </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td scope="col">1</td>
+                    <td scope="col">Sam Chua</td>
+                    <td scope="col">B37 L2 P2 Ciudad de San Jose</td>
+                    <td scope="col">200.00 PHP</td>
+                    <td scope="col">04/10/2018</td>
+                    <td scope="col"><button type="button" onclick="" class="btn btn-success">Fulfill</button></td>
+                    <td scope="col"><button type="button" onclick="" class="btn btn-danger">Reject</button></td>
+                  </tr>
+                </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  @endsection
