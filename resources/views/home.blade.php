42d7ef07f923a2684f9e8a13602a1c8163b6141e
tokii <tokiinyan@gmail.com>
2018-04-11 19:50:12 +0800

    added some strings

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Login Successful</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You are logged in! Use the navigation bar to explore the website.
                 </div>
             </div>
         </div>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,6 +5,11 @@
 @section('content')
     <div class="jumbotron text-center">
         <h1>Welcome to Lozoda!</h1>
+        @if(Auth::check())
+            <p>Click on 'Items' on the navigation bar to view what we have for you!</p>
+
+        @else
         <p>Buy all of your essentials here! To get started, please log in.</p>
+            @endif
     </div>
 @endsection
