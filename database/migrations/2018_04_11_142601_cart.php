ff70faa7d60f384da9e46512fd499315953ce74b
rigeljoseph <rigeljosephdc@gmail.com>
2018-04-11 22:36:22 +0800

    cart migration

---
+++ database/migrations/2018_04_11_142601_cart.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Cart extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart', function($table){
+            $table->string('user_id');
+            $table->string('item_id');
+            $table->string('quantity');
+            $table->string('price');
+            $table->integer('bought');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
