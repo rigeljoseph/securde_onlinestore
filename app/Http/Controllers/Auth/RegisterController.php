848226c5ddf36cd48ea3d6395010f70ed4357c7d
tokii <tokiinyan@gmail.com>
2018-04-11 20:19:36 +0800

    fixed regex so first and last name can have spaces

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,8 +51,8 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'username' => 'required|string|min:3|max:20|regex:/(^([a-zA-Z]+)(\d+)?$)/u|unique:users',
             'email' => 'required|string|email|max:255|unique:users',
-            'first_name' => 'required|string|max:255|regex:/(^([a-zA-Z]+)(\d+)?$)/u',
-            'last_name' => 'required|string|max:255|regex:/(^([a-zA-Z]+)(\d+)?$)/u',
+            'first_name' => 'required|string|max:255|regex:/^[\pL\s\-]+$/u',
+            'last_name' => 'required|string|max:255|regex:/^[\pL\s\-]+$/u',
             'password' => 'required|string|min:6|case_diff|numbers|letters|symbols|confirmed',
         ]);
     }
